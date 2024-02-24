d8f35033b6598f8acb2ec21156ed62fba496c5b4
mslee <lms980321@kakao.com>
2024-02-24 22:22:41 +0900

    'hot reload set'

--- a-nest/src/main.ts
+++ a-nest/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
   await app.listen(3000);
   console.log(`Listen on PORT : ${port}`);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+
 }
+
+
+
 bootstrap();
