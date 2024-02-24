5247ae763392849bcdac320509798187672d1bef
mslee <lms980321@kakao.com>
2024-02-24 22:14:25 +0900

    "new nest"

---
+++ a-nest/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
+  await app.listen(3000);
+  console.log(`Listen on PORT : ${port}`);
+}
+bootstrap();
